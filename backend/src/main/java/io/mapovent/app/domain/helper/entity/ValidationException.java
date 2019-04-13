1aaa56c942e1c218829d04a5b400b30c84136339
Jakub Merta <jakubm@lsnova.pl>
2019-04-13 20:59:43 +0200

    ValidationException / lombok

--- backend/src/main/java/io/mapovent/app/domain/helper/entity/ValidationException.java
+++ backend/src/main/java/io/mapovent/app/domain/helper/entity/ValidationException.java
@@ -1,20 +1,14 @@
 package io.mapovent.app.domain.helper.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.validation.ObjectError;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
 public class ValidationException extends Exception {
 
     private final List<ObjectError> objectErrorList;
-
-
-    public ValidationException(List<ObjectError> objectErrorList) {
-        this.objectErrorList = objectErrorList;
-    }
-
-    public List<ObjectError> getObjectErrorList() {
-        return objectErrorList;
-    }
-
 }
