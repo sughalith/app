cc086da1b50378869ecec4f1b9106fcba262bcac
Jakub Merta <jakubm@lsnova.pl>
2019-04-13 15:13:40 +0200

    web config  / remove message source

--- backend/src/main/java/io/mapovent/app/config/WebConfig.java
+++ backend/src/main/java/io/mapovent/app/config/WebConfig.java
@@ -1,9 +1,6 @@
 package io.mapovent.app.config;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
@@ -21,14 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("json", MediaType.APPLICATION_JSON);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
-        bean.setBasename("classpath:locale/messages");
-        bean.setDefaultEncoding("UTF-8");
-        return bean;
     }
 
 }
