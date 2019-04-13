efc2aba7485a7770d404c3e68c4540aa4c2d2604
GŁODOMOREK <pawellakomiec95@gmail.com>
2019-04-13 19:19:28 +0200

    new family tree of files, some fixes with map

--- frontend/mapovent/src/app/app.routing.ts
+++ frontend/mapovent/src/app/app.routing.ts
@@ -1,23 +1,25 @@
 import {RouterModule, Routes} from '@angular/router';
-import {AuthGuard} from './_guards/auth-guard';
 import {MapComponent} from './map/map.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {EventComponent} from './event/event.component';
+import {MainComponent} from './main/main.component';
 
 
 const appRoutes: Routes = [
-  {path: 'mapPanel', component: MapComponent, canActivate: [AuthGuard]},
+  {path: 'mapPanel', component: MapComponent},
 
-  {path: 'loginPanel', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: 'loginPanel', component: LoginComponent},
 
-  {path: 'registerPanel', component: RegisterComponent, canActivate: [AuthGuard]},
+  {path: 'registerPanel', component: RegisterComponent},
 
-  {path: 'eventPanel', component: EventComponent, canActivate: [AuthGuard]},
+  {path: 'eventPanel', component: EventComponent},
+
+  {path: 'mainPanel', component: MainComponent},
 
 
   // otherwise redirect to home
-  {path: '**', redirectTo: ''}
+  {path: '**', component: MainComponent}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
