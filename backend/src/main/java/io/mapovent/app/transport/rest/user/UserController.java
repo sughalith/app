dd1018c8824c61298c71eeff9d752afe768aed13
Jakub Merta <jakubm@lsnova.pl>
2019-04-13 12:39:51 +0200

    UserController

--- backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
+++ backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
@@ -1,4 +1,6 @@
 package io.mapovent.app.transport.rest.user;
 
-public class UserController {
+import io.mapovent.app.transport.rest.GenericController;
+
+public class UserController extends GenericController {
 }
