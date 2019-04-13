9a7603a3f3ef47ca0916fdcb73240fefd57c532c
Jakub Merta <jakubm@lsnova.pl>
2019-04-13 14:55:18 +0200

    UserController / mappings

--- backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
+++ backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
@@ -1,33 +1,46 @@
 package io.mapovent.app.transport.rest.user;
 
 import io.mapovent.app.domain.user.entity.User;
+import io.mapovent.app.domain.user.service.UserService;
 import io.mapovent.app.transport.rest.CrudController;
 import io.mapovent.app.transport.rest.GenericController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController extends GenericController implements CrudController<User> {
 
+    private UserService userService;
 
-    public ResponseEntity<User> find(String id) {
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<User> find(@PathVariable("id") String id) {
         return null;
     }
 
+    @GetMapping
     public ResponseEntity<List<User>> find(List<String> filterBy) {
         return null;
     }
 
+    @PostMapping
     public ResponseEntity<User> create(User entity) {
         return null;
     }
 
+    @PutMapping
     public ResponseEntity<User> update(String id, User entity) {
         return null;
     }
 
+    @DeleteMapping
     public void delete(String id) {
 
     }
