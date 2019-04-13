13aa9811cbc253be2e8565845b6a3f3250e6383c
Jakub Merta <jakubm@lsnova.pl>
2019-04-13 14:56:10 +0200

    User Controller / return response entities

--- backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
+++ backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
@@ -4,6 +4,7 @@ import io.mapovent.app.domain.user.entity.User;
 import io.mapovent.app.domain.user.service.UserService;
 import io.mapovent.app.transport.rest.CrudController;
 import io.mapovent.app.transport.rest.GenericController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,26 +23,27 @@ public class UserController extends GenericController implements CrudController<
     @GetMapping
     @RequestMapping("/{id}")
     public ResponseEntity<User> find(@PathVariable("id") String id) {
-        return null;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<List<User>> find(List<String> filterBy) {
-        return null;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<User> create(User entity) {
-        return null;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<User> update(String id, User entity) {
-        return null;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping
-    public void delete(String id) {
+    public ResponseEntity delete(String id) {
 
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
