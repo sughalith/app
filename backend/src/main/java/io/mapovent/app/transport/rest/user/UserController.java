ef1001eeb4f468733cf3dff33fb7348f2820682f
Jakub Merta <jakubm@lsnova.pl>
2019-04-13 14:52:11 +0200

    UserController / implement crud controller

--- backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
+++ backend/src/main/java/io/mapovent/app/transport/rest/user/UserController.java
@@ -1,6 +1,34 @@
 package io.mapovent.app.transport.rest.user;
 
+import io.mapovent.app.domain.user.entity.User;
+import io.mapovent.app.transport.rest.CrudController;
 import io.mapovent.app.transport.rest.GenericController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
-public class UserController extends GenericController {
+import java.util.List;
+
+@RestController
+public class UserController extends GenericController implements CrudController<User> {
+
+
+    public ResponseEntity<User> find(String id) {
+        return null;
+    }
+
+    public ResponseEntity<List<User>> find(List<String> filterBy) {
+        return null;
+    }
+
+    public ResponseEntity<User> create(User entity) {
+        return null;
+    }
+
+    public ResponseEntity<User> update(String id, User entity) {
+        return null;
+    }
+
+    public void delete(String id) {
+
+    }
 }
