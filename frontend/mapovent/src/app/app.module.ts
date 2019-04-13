30a467c141146c7a54d939671e867f0efccfefd4
GŁODOMOREK <pawellakomiec95@gmail.com>
2019-04-13 12:41:19 +0200

    init modules

--- frontend/mapovent/src/app/app.module.ts
+++ frontend/mapovent/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from '../map/map.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
