1428ddefab76adea6d6c80a1beb2dc0b1b46448f
Arkadiusz Stankiewicz <Arkadiusz.Stankiewicz@lhsystems.com>
2019-04-13 12:06:08 +0200

    init entity

--- backend/src/main/java/io/mapovent/app/AppApplication.java
+++ backend/src/main/java/io/mapovent/app/AppApplication.java
@@ -9,6 +9,5 @@ public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
-    
 
 }
--- backend/src/main/java/io/mapovent/app/domain/event/entity/Event.java
+++ backend/src/main/java/io/mapovent/app/domain/event/entity/Event.java
@@ -1,4 +1,22 @@
 package io.mapovent.app.domain.event.entity;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Event {
+  private String id;
+  private String description;
+  private String title;
+  private double lat;
+  private double lon;
+  private LocalDate startDate;
+  private LocalDate endDate;
+  private BigDecimal price;
 }
