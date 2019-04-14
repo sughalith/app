7ca6a6b727df5c44b0d80cb9ad81f6577510c283
GŁODOMOREK <pawellakomiec95@gmail.com>
2019-04-14 09:50:30 +0200

    inicjalizacja projektu

--- frontend/mapovent/src/app/event-list/event-list.component.ts
+++ frontend/mapovent/src/app/event-list/event-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {EventService} from '../_services/eventService';
 import {first} from 'rxjs/internal/operators';
 import {EventObject} from '../_models/event';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-event-list',
@@ -12,11 +13,17 @@ export class EventListComponent implements OnInit {
 
   events: EventObject[] = [];
 
-  constructor(private eventService: EventService) {
+  constructor(private route: ActivatedRoute,
+              private eventService: EventService,
+              private router: Router) {
   }
 
   ngOnInit() {
     this.getAllEvents();
+  }
+
+  onRowClicked(id: number) {
+    this.router.navigate(['/eventPanel', id]);
   }
 
   private getAllEvents() {
